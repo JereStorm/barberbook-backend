8e3bd01ca52fed0c766319b047e1a6b926caa10d
Jeremias Storm <jerestorm@gmail.com>
2025-09-12 19:50:14 -0300

    [Jere][Services] Se agrega class-validator para create-service-dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitamos validaciones globales
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades no definidas en DTO
+      forbidNonWhitelisted: true, // lanza error si mandan props extra
+      transform: true, // convierte automáticamente los tipos (ej: string → number)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -1,8 +1,23 @@
+import { IsNotEmpty, IsString, IsBoolean, IsNumber, Min } from 'class-validator';
 
 export class CreateServiceDto {
+    @IsNotEmpty()
+    @IsNumber()
     salonId: number;
+
+    @IsNotEmpty()
+    @IsString()
     name: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(15)
     durationMin: number;
+
+    @IsNotEmpty()
+    @IsString()
     price: string;
+
+    @IsBoolean()
     isActive?: boolean = true;
 }
